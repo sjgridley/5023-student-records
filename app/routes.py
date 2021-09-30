3842d705be339847229b482ea3a41016502ae6fc
sjgridley <88527083+sjgridley@users.noreply.github.com>
2021-09-30 17:28:57 +1000

    edits

--- app/routes.py
+++ app/routes.py
@@ -29,8 +29,9 @@ def add_student():
         student['name'] = request.form.get('student_name')
         student['english_mark'] = request.form.get('english_mark')
         student['science_mark'] = request.form.get('science_mark')
+        student['mathematics_mark'] = request.form.get('mathematics_mark')
         student['does_homework'] = request.form.get('does_homework') == 'on'
-        # TODO: Add new keys to student dictionary for new fields
+        student['stays_on_task'] = request.form.get('stays_on_task') == 'on'
 
         # Load the students from the CSV file and add the new student
         students = load_students()
@@ -38,8 +39,7 @@ def add_student():
 
         # Open up the csv file and overwrite the contents
         with open('students.csv', 'w', newline='') as file:
-            # TODO: Edit the fieldnames to include the new fields
-            fieldnames = ['name', 'english_mark', 'science_mark', 'does_homework']
+            fieldnames = ['name', 'english_mark', 'science_mark', 'mathematics_mark', 'does_homework', 'stays_on_task']
             writer = csv.DictWriter(file, fieldnames = fieldnames)
             writer.writeheader()
             writer.writerows(students)
